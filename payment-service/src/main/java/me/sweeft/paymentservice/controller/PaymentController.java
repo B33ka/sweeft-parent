f6e35186b97b34f290aeaa51421322405135cbf6
b3ka <bekasaldadze@gmail.com>
2021-06-24 11:50:10 +0400

    First commit

---
+++ payment-service/src/main/java/me/sweeft/paymentservice/controller/PaymentController.java
@@ -0,0 +1,55 @@
+package me.sweeft.paymentservice.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import me.sweeft.paymentservice.model.License;
+import me.sweeft.paymentservice.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/payment")
+public class PaymentController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+                                              @PathVariable("licenseId") String licenseId) {
+
+        License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(methodOn(PaymentController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+                linkTo(methodOn(PaymentController.class).createLicense(license)).withRel("createLicense"),
+                linkTo(methodOn(PaymentController.class).updateLicense(license)).withRel("updateLicense"),
+                linkTo(methodOn(PaymentController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense")
+        );
+
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<License> updateLicense(@RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request));
+    }
+
+    @PostMapping
+    public ResponseEntity<License> createLicense(@RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request));
+    }
+
+    @DeleteMapping(value = "/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
+    }
+}
